ea92a20d0be060fafbc1647ec674b84704d30c99
Dustin Li <dustin.li1988@gmail.com>
2022-03-14 16:16:56 +0000

    Setting up HTML page layout, linking index.html to style.css and script.js. Add icons images to assets folder. Defining early variables and functions within script.js.

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let iconsArray = []
+
+let gamespace = document.getElementById('gamespace')
+console.log(gamespace)
+
+let clickCount
+let Timer
+
+let iconsChosen = []
+let iconsChosenId = []
+let iconsMatch = []
+
+function createGame ()
+function flipIcons ()
+
+function clickCount ()
+function timer ()
+
+function easy ()
+function intermediate ()
+function hard
+
+function endGame ()
